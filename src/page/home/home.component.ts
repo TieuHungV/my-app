28ec19d9a35373d1ecfb134b4233581337d45285
TieuHung <huyteam89@gmail.com>
2018-06-28 23:51:35 +0700

    Fixbug : project not work

--- src/page/home/home.component.ts
+++ src/page/home/home.component.ts
@@ -24,15 +24,14 @@ export class HomeComponent {
   totalPages: any;
 
   constructor() {
-    console.log('home component');
     setTimeout(() => {
+      this.demmoFunction();
       this.animeShow();
-      this.animeLoading();
-    }, 2000);
+    }, 1000);
+  }
 
-    [1, 2, 3].forEach(a => {
-      console.log('hung->', a);
-    });
+  $onInit() {
+
   }
 
   demmoFunction() {
@@ -132,44 +131,4 @@ export class HomeComponent {
     logoAnimation.add(data);
   }
 
-  animeLoading() {
-    const wrapperEl = document.querySelector('.wrapper');
-    const numberOfEls = 90;
-    const duration = 6000;
-    const delay = duration / numberOfEls;
-
-    const tl = anime.timeline({
-      duration: delay,
-      complete: function () {
-        tl.restart();
-      }
-    });
-
-    function createEl(i) {
-      const el = document.createElement('div');
-      const rotate = (360 / numberOfEls) * i;
-      const translateY = -50;
-      const hue = Math.round(360 / numberOfEls * i);
-      el.classList.add('el');
-      el.style.backgroundColor = 'hsl(' + hue + ', 40%, 60%)';
-      el.style.transform = 'rotate(' + rotate + 'deg) translateY(' + translateY + '%)';
-      tl.add({
-        begin: function () {
-          anime({
-            targets: el,
-            backgroundColor: ['hsl(' + hue + ', 40%, 60%)', 'hsl(' + hue + ', 60%, 80%)'],
-            rotate: [rotate + 'deg', rotate + 10 + 'deg'],
-            translateY: [translateY + '%', translateY + 10 + '%'],
-            scale: [1, 1.25],
-            easing: 'easeInOutSine',
-            direction: 'alternate',
-            duration: duration * .1
-          });
-        }
-      });
-      wrapperEl.appendChild(el);
-    };
-
-    for (let i = 0; i < numberOfEls; i++) createEl(i);
-  }
 }
